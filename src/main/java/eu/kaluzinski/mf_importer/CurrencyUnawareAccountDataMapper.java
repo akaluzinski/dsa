4d508515de1a7f2bd2b4b1c24ec7b1b5289a8ca1
Adrian Kaluzinski <kaluza@gmail.com>
2024-05-19 11:10:55 +0200

    Extract headers from monefy import

--- src/main/java/eu/kaluzinski/mf_importer/CurrencyUnawareAccountDataMapper.java
+++ src/main/java/eu/kaluzinski/mf_importer/CurrencyUnawareAccountDataMapper.java
@@ -1,13 +1,32 @@
 package eu.kaluzinski.mf_importer;
 
+import eu.kaluzinski.mf_importer.emums.Header;
+import eu.kaluzinski.mf_importer.model.AccountEntry;
 import eu.kaluzinski.mf_importer.model.AccountState;
 import java.util.List;
 
 public class CurrencyUnawareAccountDataMapper implements AccountDataMapper {
 
   public AccountState mapImportDataToAccountState(List<List<String>> rawImportData) {
+    var headers = mapRawImportRowToHeaders(rawImportData.get(0));
 
+    var accountEntries = rawImportData.stream().map(
+        rawEntry -> mapRawImportRowToAccountEntry(rawEntry)
+    ).toList();
+
+    var accountState = new AccountState(accountEntries, accountEntries);
+    return accountState;
+  }
+
+  private AccountEntry mapRawImportRowToAccountEntry(List<String> rawImportRow) {
     return null;
+  }
+
+  private List<Header> mapRawImportRowToHeaders(List<String> rawImportRow) {
+    return rawImportRow.stream()
+        .map(String::toUpperCase)
+        .map(cell -> cell.replaceAll(" ", "_"))
+        .map(Header::valueOf).toList();
   }
 
 
---
+++ src/main/java/eu/kaluzinski/mf_importer/emums/Header.java
@@ -0,0 +1,27 @@
+package eu.kaluzinski.mf_importer.emums;
+
+public enum Header {
+
+  DATE("date"),
+  ACCOUNT("account"),
+  AMOUNT("amount"),
+  CATEGORY("category"),
+  CURRENCY("currency"),
+  CONVERTED_AMOUNT("converted amount"),
+  CONVERTED_CURRENCY("currency"),
+  DESCRIPTION("description");
+
+
+  private String name;
+
+  Header(String name) {
+    this.name = name;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+
+}
