fcf57451f965c748556a5ba70970fd0e88aa20a0
Adrian Kaluzinski <kaluza@gmail.com>
2024-06-15 11:52:59 +0200

    Convert maven to gradle. Add Spring Boot support.

---
+++ src/test/java/eu/kaluzinski/monefyinsights/MonefyinsightsApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.kaluzinski.monefyinsights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonefyinsightsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
